2647413015bd6d7a7ca02e7d34b6e731fb9a352e
lurb3 <gustavo.gigante.s@gmail.com>
2020-08-09 16:13:19 +0100

    Working version of graphql with mysql

--- api/db/user.js
+++ api/db/user.js
@@ -1,18 +1,7 @@
 const createUserModel = db => {
     return {
         findAll() {
-            function getRecord() {
-                return new Promise(function(resolve) {
-                    db.query("SELECT username FROM users", function(erro, rows, fields) {
-                        if (erro) {
-                            return reject(erro);
-                        }
-                        return resolve(rows);
-                    })
-                })
-            }
-            getRecord().then((result)=> result)
-
+            
         }
     }
 }
--- api/server.js
+++ api/server.js
@@ -1,21 +1,64 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const { models, db } = require('./db')
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+var mysql = require('mysql');
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context() {
-        return { models, db }
+var schema = buildSchema(`
+    type User {
+        id: String
+        name: String
+        username: String
+        email: String
     }
-})
+    type Query {
+      getUsers: [User],
+      getUserInfo(id: Int) : User
+    }    
+    type Mutation {
+        updateUserInfo(id: Int, name: String, email: String, username: String) : Boolean
+        createUser(name: String, email: String, username: String) : Boolean
+        deleteUser(id: Int) : Boolean
+    }
+`);
 
-console.log(models.User.findAll())
+const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
+    req.mysqlDb.query(sql, args, (err, rows) => {
+        if (err)
+            return reject(err);
+        rows.changedRows || rows.affectedRows || rows.insertId ? resolve(true) : resolve(rows);
+    });
+});
 
-server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`)
-})
+const root = {
+    getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
+    getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
+    updateUserInfo: (args, req) => queryDB(req, "update users SET ? where id = ?", [args, args.id]).then(data => data),
+    createUser: (args, req) => queryDB(req, "insert into users SET ?", args).then(data => data),
+    deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data)
+  };
+
+var app = express();
+
+app.use((req, res, next) => {
+    req.mysqlDb = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'test'
+    });
+    req.mysqlDb.connect();
+    next();
+  });
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+
+console.log('Running a GraphQL API server at localhost:4000/graphql');
 
 /*
 const express = require('express')
